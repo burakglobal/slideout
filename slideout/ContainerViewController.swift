18f3df3236271f723acd71fe9e27b462141c868a
Dennis Suratna <dennis.suratna@gmail.com>
2015-08-05 11:37:03 -0700

    Init

---
+++ slideout/ContainerViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  slideout
+//
+//  Created by Dennis Suratna on 8/5/15.
+//  Copyright (c) 2015 Dennis Suratna. All rights reserved.
+//
+
+import UIKit
+
+class ContainerViewController: UIViewController {
+
+}
+
