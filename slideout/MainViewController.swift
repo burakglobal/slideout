518e35884731d15f7dacbdb2c91804edf1ee6fb3
Dennis Suratna <dennis.suratna@gmail.com>
2015-08-07 14:41:03 -0700

    Update

---
+++ slideout/MainViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MainViewController.swift
+//  slideout
+//
+//  Created by Dennis Suratna on 8/7/15.
+//  Copyright (c) 2015 Dennis Suratna. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+}
